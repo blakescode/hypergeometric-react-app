49945eaa5096c9b02466e38eeb21a1f78628cc9b
blakescode <rydyell8@gmail.com>
2021-10-24 17:40:57 +0100

    simple form with material UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack'
+
+
 
 function App() {
+  const [state, setState] = useState({
+    populationSize: '',
+    populationSuccesses: '',
+    sampleSize: '',
+    sampleSuccesses: ''
+  })
+
+  const handleFormChange = (event) => {
+    const updatedValue = event.target.value;
+    setState({
+      ...state,
+      [event.target.name]: updatedValue
+    });
+  }
+
+  const calculate = () => {
+    console.log('population size = ', state.populationSize)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1>Hypergeometric Calculator</h1>
+      </div>
+      <div className="Form">
+        <Stack spacing={2} className='stack-form'>
+          <TextField 
+            id="populationSize"
+            name="populationSize"
+            label="Population Size"
+            variant="outlined"
+            value={state.populationSize}
+            onChange={handleFormChange}
+          />
+          <TextField 
+            id="populationSuccesses"
+            name="populationSuccesses"
+            label="Population Successes"
+            variant="outlined"
+            value={state.populationSuccesses}
+            onChange={handleFormChange}
+          />
+          <TextField 
+            id="sampleSize"
+            name="sampleSize"
+            label="Sample Size"
+            variant="outlined"
+            value={state.sampleSize}
+            onChange={handleFormChange}
+          />
+          <TextField 
+            id="sampleSuccesses"
+            name="sampleSuccesses"
+            label="Sample Successes"
+            variant="outlined"
+            value={state.sampleSuccesses}
+            onChange={handleFormChange}
+          />
+          <Button
+            variant="contained"
+            onClick={calculate}
+          >
+            Calculate
+          </Button>
+        </Stack>
+      </div>
     </div>
   );
 }
